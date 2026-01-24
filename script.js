24f3eb9a53cf3605026193f6ff51948355b81242
dimakunnechinedu-alt <dimakunnechinedu@gmail.com>
2026-01-24 13:33:45 -0800

    change backgroud-colors and some effect on the write-up

---
+++ script.js
@@ -0,0 +1,42 @@
+const texts = [
+  "I am a Front-End Developer.",
+  " I create exicting stuff on the internet."
+];
+
+const speeds = 70;
+const pauseAfterAll = 2000;
+
+let textIndex = 0;
+let charIndex = 0;
+let output = "";
+
+function typeEffect() {
+  const el = document.getElementById("typeText");
+
+  if (charIndex < texts[textIndex].length) {
+    output += texts[textIndex].charAt(charIndex);
+    el.innerHTML = output;
+    charIndex++;
+    setTimeout(typeEffect, speeds);
+  } 
+  else {
+    output += "<br>";
+    el.innerHTML = output;
+
+    textIndex++;
+    charIndex = 0;
+
+    if (textIndex === texts.length) {
+      setTimeout(() => {
+        output = "";
+        textIndex = 0;
+        el.innerHTML = "";
+        typeEffect();
+      }, pauseAfterAll);
+    } else {
+      setTimeout(typeEffect, speeds);
+    }
+  }
+}
+
+typeEffect();
